a1ec2b6efd507f0ec4226f44ba2e810060c97b0a
Ari <ari@Aris-iMac.lxtnkyaa.metronetinc.net>
2021-02-02 21:33:26 -0500

    updated body-parser code

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(express.bodyParser());
+const jsonParser = bodyParser.json()
 
-app.get('/on', function(req, res) {
+app.get('/on', jsonParser, function(req, res) {
   console.log(req.body);
   res.send('Turned LED on!!');
   rpio.write(37, rpio.HIGH);
 })
 
-app.get('/off', function(req, res) {
+app.get('/off', jsonParser, function(req, res) {
   res.send('Turned LED off!!');
   rpio.write(37, rpio.LOW);
 })
