e65fffefd7fa69f09c276de015880bf1a02eab5d
Ari <arimitsu0803@gmail.com>
2021-02-12 17:57:25 -0500

    Debugging

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/pin_state', jsonParser, function(req, res) {
 
 app.post('/pin_pwm', jsonParser, function(req, res) {
   console.log(req.body.state + " : " + typeof(req.body.state));
-  rpio.pwmSetData(33, req.body.state.parseInt());
+  rpio.pwmSetData(33, parseInt(req.body.state));
   res.json({ value: req.body.state, status: 'success'});
 })
 
